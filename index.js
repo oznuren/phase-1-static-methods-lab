6379f47f54a901a8227de9f1bdafbba74f08ef81
Öznur <oznur@tuta.io>
2022-06-12 14:36:19 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,3 +1,36 @@
 class Formatter {
   //add static methods here
+  static capitalize(str) {
+    return str.charAt(0).toUpperCase() + str.slice(1);
+  }
+
+  static sanitize(str) {
+    return str.replace(/[^a-zA-Z0-9-' ]/gi, "");
+  }
+
+  static titleize(title) {
+    let exception = [
+      "the",
+      "a",
+      "an",
+      "but",
+      "of",
+      "and",
+      "for",
+      "at",
+      "by",
+      "from",
+    ];
+    let arr = title.split(" ");
+
+    return arr
+      .map((word, i) => {
+        if (exception.includes(word) && i != 0) {
+          return word;
+        } else {
+          return word[0].toUpperCase() + word.slice(1);
+        }
+      })
+      .join(" ");
+  }
 }
